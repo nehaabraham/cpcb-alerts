0fc820e2e5cd33006494f9749a3072956a3fa4bd
Neha Abraham <me@nehabraham.com>
2017-01-06 15:05:42 -0500

    add phone to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :phone, format: { with: /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/ }, :allow_blank => true
 
   after_create :send_welcome_email
+  before_save :format_phone
 
   private
 
     def send_welcome_email
       AppMailer.welcome(self).deliver
+    end
+
+    def format_phone
+      self.phone.gsub!('-','')
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
+
+  devise_for :users, :controllers => { registrations: 'registrations' } # Use custom class for registration
   resources :events
   root 'static_pages#home'
 end
