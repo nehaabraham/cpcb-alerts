220892e2f83c94e9cce60e5e0085a7a8a1bc9188
Neha Abraham <me@nehabraham.com>
2017-01-06 14:15:40 -0500

    configure mailing for development

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,9 +1,7 @@
 class AppMailer < ApplicationMailer
-  default from: 'neha.abraham.pitt@gmail.com'
 
   def welcome(user)
     @user = user
-    @url  = 'http://localhost:3000/'
     mail(to: @user.email, subject: 'Welcome to CPCB Alerts')
   end
 
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'dcb@pitt.edu'
   layout 'mailer'
 end
